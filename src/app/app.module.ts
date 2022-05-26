0de456a38fff1ad333d37ba19422aeeb5c93ad7a
Alex Tsague <alextsague98@gmail.com>
2022-05-26 20:08:35 +0200

    Inscription terminée

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
-
+import {ReactiveFormsModule} from "@angular/forms";
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,8 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form:FormGroup  = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    role: new FormControl('')
+  })
+  submitted =false;
+  roles = ['Guerrier','Alchimiste','Sorcier','Espions','Enchanteur']
+  constructor(private  formBuilder:FormBuilder,private router: Router) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        username: [
+          '',
+
+          Validators.required
+        ],
+        password: [
+          '',
+
+          Validators.required
+
+        ],
+        role: [
+          '',
+          Validators.required
+        ],
+      }
+    )
+  }
+
+  get f(): { [key:string] : AbstractControl}{
+    return this.form.controls;
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.form.invalid){
+      return;
+    }
+
+    let init = {
+      method:'POST',
+      body: new Blob([JSON.stringify(this.form.value)],{type:'application/json'})
+    }
+
+    fetch('http://localhost:5000/register',init)
+      .then(response => {
+        response.json()
+          .then( data => {
+              this.router.navigate(['/'])
+          })
+      })
+
+  }
+
+  onReset(): void{
+    this.submitted = false;
+    this.form.reset();
   }
 
 }
